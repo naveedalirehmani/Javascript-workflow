68ec9715e230e855ddb81d912e30a7587aa985a0
naveedalirehmani <naveedalirehmani321@gmail.com>
2024-11-12 00:09:59 +0500

    webpack example config

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   //       exclude: /node_modules/
   //     },
   //     {
-  //       test: /\.js$/,
+  //       test: /\.jsx$/,
   //       exclude: /node_modules/,
   //       use: {
   //         loader: 'babel-loader',
