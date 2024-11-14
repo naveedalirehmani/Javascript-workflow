f272d19e78338b63d9848d50bd5d58fa21e47556
naveedalirehmani <naveedalirehmani321@gmail.com>
2024-11-14 15:39:39 +0500

    add operator.

--- nextGen/app.js
+++ nextGen/app.js
@@ -10,3 +10,8 @@
 // 1. it will implicitly convert all the operands to boolean.
 // or operator will always try to find first truthly value and return it.
 // if not found it will return last value in the chain.
+
+//? &&
+// 2. it will implicitly convert all the operands to boolean.
+// and operator will always try to find first falsy value, if not found
+// it will return the last value in the chain.
