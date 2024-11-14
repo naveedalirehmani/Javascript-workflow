1af321efd822235e4f923df04463d0f40152d023
naveedalirehmani <naveedalirehmani321@gmail.com>
2024-11-14 15:39:54 +0500

    class on is array sorted

--- nextGen/app.js
+++ nextGen/app.js
@@ -15,3 +15,27 @@
 // 2. it will implicitly convert all the operands to boolean.
 // and operator will always try to find first falsy value, if not found
 // it will return the last value in the chain.
+
+// Write a function that checks whether a given array is sorted in ascending order. The function should:
+
+// 1. Accept an array of integers as input.
+// 2. Return `true` if the array is sorted in strictly ascending order.
+// 3. Return `false` otherwise.
+
+// function isArrayIsSorted(array){
+
+// }
+
+// Understanding the problem.
+// Can I restate the problem?
+//  write a function that will take a array of integers as input, the array input will not be empty, we need to return true if the array is sorted, false if not.
+
+// what are the inputs that go into the problem?
+//  an array of integers.
+
+// what are the expected output to the problem?
+//  true if the input array is sorted.
+//  false if the input is not sorted.
+
+// Can the output be derived from the input?
+//  yes.
