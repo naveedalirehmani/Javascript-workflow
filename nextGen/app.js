8287a3f1b631d021b3f155c6761fa9072bf31d22
naveedalirehmani <naveedalirehmani321@gmail.com>
2024-11-14 15:39:26 +0500

    or

--- nextGen/app.js
+++ nextGen/app.js
@@ -5,3 +5,8 @@
 //? what are the expected output to the problem?
 //? Can the output be derived from the input?
 //? Labelling the important pieces.
+
+//? ||
+// 1. it will implicitly convert all the operands to boolean.
+// or operator will always try to find first truthly value and return it.
+// if not found it will return last value in the chain.
